11eb7f3d3b0caa06c88aea0bfc4b3ad1f584da3d
michielbrinkers <contact@michielbrinkers.nl>
2025-12-07 00:03:24 -0600

    Day 7

---
+++ 2025/day_7/day7a.py
@@ -0,0 +1,18 @@
+# https://adventofcode.com/2025/day/6
+use_example = False
+with open("input7.txt" if not use_example else "example7.txt", "r") as file:
+	lines = file.read().splitlines()[::2] # even lines only, because we don't need the ... lines
+
+beams = set([lines.pop(0).find("S")])
+print(lines, beams)
+
+split_count = 0
+for line in lines:
+	splits = [i for i,ch in enumerate(line) if ch == "^"]
+	for b in beams.copy():
+		if b in splits:
+			beams.remove(b)
+			beams.add(b-1)
+			beams.add(b+1)
+			split_count += 1
+print(split_count)
---
+++ 2025/day_7/day7b.py
@@ -0,0 +1,16 @@
+# https://adventofcode.com/2025/day/6
+use_example = False
+with open("input7.txt" if not use_example else "example7.txt", "r") as file:
+	lines = file.read().splitlines()[::2] # even lines only, because we don't need the ... lines
+
+beams = { lines.pop(0).find("S") : 1 }
+
+for line in lines:
+	splits = [i for i,ch in enumerate(line) if ch == "^"]
+	for b,c in list(beams.items()):
+		if b in splits:
+			del beams[b]
+			beams[b-1] = beams.get(b-1,0) + c  
+			beams[b+1] = beams.get(b+1,0) + c  
+
+print(sum(beams.values()))
